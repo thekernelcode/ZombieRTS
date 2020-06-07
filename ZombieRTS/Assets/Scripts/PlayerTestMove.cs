946a1ca7b7d09d95597f1e744c2f5047ba2cd185
thekernelcode <45094334+thekernelcode@users.noreply.github.com>
2020-06-07 22:05:54 +0100

    Added tree asset
    Also changed some materials

---
+++ ZombieRTS/Assets/Scripts/PlayerTestMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTestMove : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dirToMove = target.position - transform.position;
+        transform.Translate(dirToMove.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.3f)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if (waypointIndex >= Waypoints.waypoints.Length)
+        {
+            waypointIndex = 0;
+            return;
+        }
+
+        target = Waypoints.waypoints[waypointIndex];
+
+        waypointIndex++;
+    }
+}
